83d4d6ec3a640ff38a7811e89696b42c0f328b61
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-11 20:53:26 +0200

    exercise 2.9 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,7 +10,7 @@ const Numbers = ({persons}) => {
   return(
     <>
       {persons.map((person) => 
-          <Number key={person.name} name={person.name} number={person.number} />
+          <Number key={person.id} name={person.name} number={person.number} />
       )}
     </>
   )
@@ -36,20 +36,26 @@ const Form = ({onSubmit, name, number, onNameChange, onNumberChange}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas",
-      number: "040-1234567"
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filteredName, setFilteredName] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
-
   }
+
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    const eventValue = event.target.value
+    setFilteredName(eventValue)
   }
 
   const addName = (event) => {
@@ -73,16 +79,23 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
   }
 
+  const personsToShow = (filteredName) !== ""
+    ? persons.filter(person => person.name.toLowerCase().includes(filteredName.toLowerCase()))
+    : persons
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+        filter shown with <input onChange={handleFilterChange}/>
+      <h2>Add a new person</h2>
       <Form 
         onSubmit={addName} 
         name={newName} 
@@ -90,7 +103,7 @@ const App = () => {
         onNameChange={handleNameChange} 
         onNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={personsToShow} />
     </div>
   )
 }
