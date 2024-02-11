5fe0246522dd772dffd1479b171a25290a60ace2
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-11 18:30:34 +0200

    exercise 2.5 completed

--- part2/courseinformation/src/App.jsx
+++ part2/courseinformation/src/App.jsx
@@ -1,57 +1,4 @@
-const Header = ({name}) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-
-const Part = ({name, exercises}) => {
-  return(
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map(part => 
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </>
-  )
-}
-
-const Total = ({parts}) => {
-  const initialExercise = 0
-  const totalExercise = parts.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.exercises,
-    initialExercise
-  )
-  return (
-    <p><b>Total of {totalExercise} exercises</b></p>
-  )
-}
-
-const Course = ({course}) => {
-  return(
-    <div id={"course-" + course.id}>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Courses = ({courses}) => {
-  return(
-    <>
-      {courses.map(course => 
-        <Course key={course.id} course={course} />
-      )}
-    </>
-  )
-}
+import Courses from "./components/Courses"
 
 const App = () => {
   const courses = [
@@ -98,6 +45,7 @@ const App = () => {
       ]
     }
   ]
+  
   return (
     <>
       <h1>Web development curriculum</h1>
