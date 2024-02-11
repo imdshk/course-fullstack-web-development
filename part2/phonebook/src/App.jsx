5db050bdc87c34449b22a7e3a80c1f0f45e56150
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-11 20:17:20 +0200

    exercise 2.8 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-const Number = ({name}) => {
+const Number = ({name, number}) => {
   return(
-    <p>{name}</p>
+    <p>{name} {number}</p>
   )
 }
 
@@ -10,17 +10,20 @@ const Numbers = ({persons}) => {
   return(
     <>
       {persons.map((person) => 
-          <Number key={person.name} name={person.name} />
+          <Number key={person.name} name={person.name} number={person.number} />
       )}
     </>
   )
 }
 
-const Form = ({onSubmit, value, onChange}) => {
+const Form = ({onSubmit, name, number, onNameChange, onNumberChange}) => {
   return(
     <form onSubmit={onSubmit} >
       <div>
-        name: <input value={value} onChange={onChange} />
+        name: <input value={name} onChange={onNameChange} />
+      </div>
+      <div>
+        number: <input value={number} onChange={onNumberChange} />
       </div>
       <div>
         <button type="submit">
@@ -33,32 +36,59 @@ const Form = ({onSubmit, value, onChange}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: "Arto Hellas"}
+    {
+      name: "Arto Hellas",
+      number: "040-1234567"
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addName = (event) => {
     event.preventDefault()
     const isNameInPersons = persons.find(({name}) => name === newName)
+    if(newName === "" || newNumber === ""){
+      alert("name or number is empty")
+      return
+    }
+    
     if(typeof(isNameInPersons) !== "undefined"){
       alert(`${newName} is already added to phonebook`)
       return
     }
-    const nameObject = {
-      name: newName
+
+    const isNumberInPersons = persons.find(({number}) => number === newNumber)
+    if(typeof(isNumberInPersons) !== "undefined"){
+      alert(`${newNumber} is already added to phonebook`)
+      return
     }
-    setPersons(persons.concat(nameObject))
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Form onSubmit={addName} value={newName} onChange={handleNameChange}/>
+      <Form 
+        onSubmit={addName} 
+        name={newName} 
+        number={newNumber} 
+        onNameChange={handleNameChange} 
+        onNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
       <Numbers persons={persons} />
     </div>
